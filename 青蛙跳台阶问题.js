89fd32f2f38b00b4fd88ba7ef7b88ac87d6b6ee2
haojie04 <haojie04@maoyan.com>
2021-03-15 22:17:21 +0800

    青蛙跳台阶问题

--- 青蛙跳台阶问题.js
+++ 青蛙跳台阶问题.js
@@ -3,19 +3,21 @@
  * @return {number}
  */
 const cache = {};
-var fib = function(n) {
+var numWays = function(n) {
   if (cache[n] !== undefined) {
     return cache[n];
   };
   if (n === 0) {
-    return 0;
+    return 1;
   }
   if (n === 1) {
     return 1;
   }
-  const result = fib(n - 1) + fib(n - 2);
+  const result = numWays(n - 1) + numWays(n - 2);
   if (cache[n] === undefined) {
     cache[n] = result;
   }
   return result % 1000000007;
 };
+
+console.log(numWays(78));
