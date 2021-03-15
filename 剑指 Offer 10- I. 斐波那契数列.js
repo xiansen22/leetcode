f40e34158209df5f95f2fdeac431d985905af507
haojie04 <haojie04@maoyan.com>
2021-03-15 21:47:09 +0800

    fib

---
+++ 剑指 Offer 10- I. 斐波那契数列.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const cache = {};
+var fib = function(n) {
+  if (cache[n] !== undefined) {
+    return cache[n];
+  };
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  const result = fib(n - 1) + fib(n - 2);
+  if (cache[n] === undefined) {
+    cache[n] = result;
+  }
+  return result % 1000000007;
+};
